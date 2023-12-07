f8f657dcc3dcfec82ae8feaab29d24c066aca562
balibabu <balibabuchauhan1@gmail.com>
2023-12-07 14:59:09 +0545

    view:

--- todo/views.py
+++ todo/views.py
@@ -3,9 +3,6 @@ from .models import Todo
 from .serializers import TodoSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from rest_framework import status
 
 
 class TodoListCreateView(generics.ListCreateAPIView):
