78007dfeb92b1fcbb1c6b8a16317aff221bc482e
balibabu <balibabuchauhan1@gmail.com>
2023-12-11 23:29:52 +0545

    added color option

--- notepad/models.py
+++ notepad/models.py
@@ -6,5 +6,6 @@ class Note(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     title=models.CharField(max_length=255)
     description=models.TextField()
+    color=models.CharField(max_length=20,default='#dcdcdc')
     created_time = models.DateTimeField(auto_now_add=True)
     
--- notepad/serializers.py
+++ notepad/serializers.py
@@ -4,5 +4,5 @@ from .models import Note
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ('id', 'user', 'title', 'description', 'created_time')
+        fields = ('id', 'user', 'title', 'description', 'created_time','color')
         read_only_fields = ['user']
